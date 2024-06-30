d2d076050054cfacf9d341aa724fbba51f660096
euphyfang <ea103t41@gmail.com>
2024-06-30 18:03:45 +0800

    create login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
   {
-    path: '',
+    path: 'login',
     component: HomeComponent
-  }
+  },
+  { path: 'logout', component: HomeComponent },
+  { path: '404', component: HomeComponent },
+  { path: '**', redirectTo: '404' }
 ];
